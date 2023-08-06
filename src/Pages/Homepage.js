c60b40c179bbd917262259fd0152ba7ba34f2a70
Fardeen Ahamed <1si20et403@sit.ac.in>
2023-08-06 17:48:59 +0530

    authentication and signUp is added and connected to firebbase and stored the data in firebase for authentication

---
+++ src/Pages/Homepage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import StartingPageContent from "../Components/StartingPage/StartingPageContent";
+
+const Homepage = () => {
+  return <StartingPageContent />;
+};
+
+export default Homepage;
---
+++ src/Pages/ProfilePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import UserProfile from "../Components/Profile/UserProfile";
+
+const ProfilePage = () => {
+  return <UserProfile />;
+};
+
+export default ProfilePage;
